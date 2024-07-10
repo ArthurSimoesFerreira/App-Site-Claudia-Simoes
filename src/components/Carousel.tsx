82a9bb182ad45cabb00a127eb48f164e45546628
Arthur Simões Ferreira <asferreira@id.uff.br>
2024-07-10 04:35:50 -0300

    feat: implemented auto window scroll when the carousel is clicked

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,24 +1,33 @@
-import carousel_1 from "/carousel_1.png"
-import carousel_2 from "/carousel_2.png"
-import carousel_3 from "/carousel_3.png"
+import React from 'react';
+import carousel_1 from "/carousel_1.png";
+import carousel_2 from "/carousel_2.png";
+import carousel_3 from "/carousel_3.png";
 import { Link } from "react-router-dom";
+
+const scrollDown = () => {
+    window.scrollBy({
+        top: 600,
+        left: 0,
+        behavior: 'smooth'
+    });
+};
 
 const Carousel = () => {
     return (
         <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <Link to="/">
+                    <Link to="/" onClick={scrollDown}>
                         <img src={carousel_1} className="d-block w-100" style={{ maxHeight: "660px" }} alt="carousel_image_1" />
                     </Link>
                 </div>
                 <div className="carousel-item">
-                    <Link to="/top">
+                    <Link to="/top" onClick={scrollDown}>
                         <img src={carousel_2} className="d-block w-100" style={{ maxHeight: "660px" }} alt="carousel_image_2" />
                     </Link>
                 </div>
                 <div className="carousel-item">
-                    <Link to="/camisa">
+                    <Link to="/camisa" onClick={scrollDown}>
                         <img src={carousel_3} className="d-block w-100" style={{ maxHeight: "660px" }} alt="carousel_image_3" />
                     </Link>
                 </div>
